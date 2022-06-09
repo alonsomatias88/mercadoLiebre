942e0057dc94ca7620ca75d3dcb4a27286486170
alonsomatias88 <alonso.matias@live.com.ar>
2022-06-09 20:24:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(2020, () => console.log("Server is running on LocalHost 2020"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/home.html"))
+);
+const public = path.resolve(__dirname, "./public");
+app.use(express.static(public));
+
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/register.html"))
+);
+const register = path.resolve(__dirname, "./register");
+app.use(express.static(register));
+
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/login.html"))
+);
+const login = path.resolve(__dirname, "./login");
+app.use(express.static(login));
